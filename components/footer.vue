3e9da09f4be4454917e5dda06218800ea5ddb33f
吉田拓未 <takumi.yoshida.seattle@gmail.com>
2020-03-18 02:30:24 +0900

    introduces footer components

---
+++ components/footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        メールでの連絡がつきやすいです．経歴の詳細などはlinkedInをご参照ください．
+        また，お仕事のご依頼は現在受け付けておりません．予めご了承ください．
+        メールでの連絡がつきやすいです．経歴の詳細などはlinkedInをご参照ください．
+        また，お仕事のご依頼は現在受け付けておりません．予めご了承ください．
+        メールでの連絡がつきやすいです．経歴の詳細などはlinkedInをご参照ください．
+      </v-card-text>
+
+      <v-divider />
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+import '@fortawesome/fontawesome-free/css/all.css'
+export default {
+  data: () => ({
+    icons: [
+      'fab fa-github',
+      'fab fa-google',
+      'fab fa-linkedin',
+      'fab fa-instagram'
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
