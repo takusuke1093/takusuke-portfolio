29a6857139329d67c0235a407d2a6318e6d79323
吉田拓未 <takumi.yoshida.seattle@gmail.com>
2020-03-18 02:28:31 +0900

    fixed index.vue profile

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,331 @@
+
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
+  <div id="app">
+    <v-app-bar
+      app
+      color="white"
+      height="100"
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
+      <v-avatar
+        tile
+        class="mr-3"
+        size="70"
+      >
+        <v-img
+          contain
+          max-height="100%"
+          :src="require('@/assets/image/logo_150_ppi.png')"
+        />
+      </v-avatar>
+
+      <v-toolbar-title class="font-weight-black headline">
+        VUETIFY
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <section id="hero">
+        <v-row no-gutters>
+          <v-img
+            :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+            :src="require('@/assets/image/home.jpg')"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  align="center"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-col
+                    class="white--text text-center"
+                    cols="12"
+                    tag="h1"
+                  >
+                    <span
+                      :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                      class="font-weight-black"
+                    >
+                      @takusuke1093
+                    </span>
+                  </v-col>
+
+                  <v-btn
+                    class="align-self-end"
+                    fab
+                    outlined
+                    @click="$vuetify.goTo('#about-me')"
+                  >
+                    <v-icon>mdi-chevron-double-down</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+          </v-img>
+        </v-row>
+      </section>
+
+      <section id="about-me">
+        <div class="py-12" />
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">
+            ABOUT ME
+          </h2>
+
+          <v-responsive
+            class="mx-auto mb-8"
+            width="56"
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+            <v-divider class="mb-1" />
+
+            <v-divider />
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
+          >
+            @takusuke1093<br>
+            Web系の主にフロントエンドを担当しています．<br>
+            Vue.js, Ruby on Rails でお仕事してます．<br>
+            写真撮るのが好きです（上手いとは言ってない）<br>
+            最近は朝活しようと検討中ですが全く実行できてなくて泣き
+          </v-responsive>
+
+          <v-avatar
+            class="elevation-12 mb-12"
+            size="128"
+          >
+            <v-img :src="require('@/assets/image/profile.jpg')" />
+          </v-avatar>
+
+          <div />
+
           <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            v-for="[icon, src] of icons"
+            :key="icon"
+            :src="src"
+            class="mx-4 white--text"
+            icon
           >
-            Continue
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+        </v-container>
+
+        <div class="py-12" />
+      </section>
+
+      <section
+        id="features"
+        class="grey lighten-3"
+      >
+        <div class="py-12" />
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3 font--black">
+            SKILLS
+          </h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1" />
+
+            <v-divider />
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ icon, title, text }, i) in features"
+              :key="i"
+              cols="12"
+              md="4"
+              color="indigo lighten-1"
+            >
+              <v-card
+                class="py-12 px-4"
+                color="white"
+                flat
+              >
+                <v-theme-provider dark>
+                  <div>
+                    <v-avatar
+                      color="primary"
+                      size="88"
+                    >
+                      <v-icon
+                        large
+                        v-text="icon"
+                      />
+                    </v-avatar>
+                  </div>
+                </v-theme-provider>
+
+                <v-card-title
+                  class="justify-center font--black text-uppercase"
+                  v-text="title"
+                />
+
+                <v-card-text
+                  class="subtitle-1"
+                  v-text="text"
+                />
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12" />
+      </section>
+
+      <section id="stats">
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+          :src="require('@/assets/image/_DSC4594.jpg')"
+        >
+          <v-container fill-height>
+            <v-row class="mx-auto">
+              <v-col
+                v-for="[value, title] of stats"
+                :key="title"
+                cols="12"
+                md="3"
+              >
+                <div class="text-center">
+                  <div
+                    class="display-3 font-weight-black mb-4"
+                    v-text="value"
+                  />
+
+                  <div
+                    class="title font-weight-regular text-uppercase"
+                    v-text="title"
+                  />
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-parallax>
+      </section>
+
+      <section id="blog">
+        <div class="py-12" />
+
+        <v-container>
+          <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+            Blog
+          </h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1" />
+
+            <v-divider />
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ src, text, title }, i) in articles"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="src"
+                class="mb-4"
+                height="275"
+                max-width="100%"
+              />
+
+              <h3
+                class="font-weight-black mb-4 text-uppercase"
+                v-text="title"
+              />
+
+              <div
+                class="title font-weight-light mb-5"
+                v-text="text"
+              />
+
+              <v-btn
+                class="ml-n4 font-weight-black"
+                text
+              >
+                Continue Reading
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12" />
+      </section>
+    </v-content>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data () {
+    return {
+      articles: [
+        {
+          src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          title: 'Mobile first & Responsive',
+          text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          title: 'Think outside the box',
+          text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
+          title: 'Small changes, big difference',
+          text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.'
+        }
+      ],
+      features: [
+        {
+          icon: 'mdi-account-group-outline',
+          title: 'Vibrant Community',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam'
+        },
+        {
+          icon: 'mdi-update',
+          title: 'Frequent Updates',
+          text: 'Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit.'
+        },
+        {
+          icon: 'mdi-shield-outline',
+          title: 'Long-term Support',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam'
+        }
+      ],
+      stats: [
+        ['24k', 'Github Stars'],
+        ['330+', 'Releases'],
+        ['1m', 'Downloads/mo'],
+        ['5m', 'Total Downloads']
+      ],
+      icons: [
+        ['fab fa-github', 'https://github.com/takusuke1093'],
+        ['fab fa-google', 'https://github.com/takusuke1093'],
+        ['fab fa-linkedin', 'https://github.com/takusuke1093'],
+        ['fab fa-instagram', 'https://github.com/takusuke1093']
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .font--black{
+    color: #121212;
+  }
+</style>
