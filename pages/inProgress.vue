59cd7d99d60437f1e4f637874894ab263f7b1209
吉田拓未 <takumi.yoshida.seattle@gmail.com>
2020-03-24 04:32:58 +0900

    add inProgress.vue. delete inspire.vue(template)

---
+++ pages/inProgress.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout>
+    <v-card class="mx-auto yellow darken-2">
+      <v-card-title color="yellow darken-2">
+        Sorry!
+      </v-card-title>
+      <v-card-text>   There might be a page once I start building Blogs</v-card-text>
+      <v-card />
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'inprogress'
+}
+</script>
+
+<style >
+.v-content__wrap {
+  background-color : white;
+}
+</style>
